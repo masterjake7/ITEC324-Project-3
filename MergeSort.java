76c80b0cb22a88c74aff0a84afad6aac52b770b5
HaynerS <shaun.hayner@hotmail.com>
2015-04-08 19:06:57 -0400

    Merge Sort
    Added correct sort code into MergeSort class. JProgressBar isn't
    updating correctly at this time.

--- MergeSort.java
+++ MergeSort.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.swing.JProgressBar;
 
 public class MergeSort implements Runnable {
@@ -12,61 +11,106 @@ public class MergeSort implements Runnable {
 	private int numberOfPasses = -1;
 	private long startTime, finishTime, timeElapsed;
 	private JProgressBar bar;
-	
+
 	public MergeSort(Integer[] data, JProgressBar bar)
 	{
 		this.data = data;
 		this.bar = bar;
 	}
-	
+
 	public void run()
 	{
 		startTime = System.nanoTime();
-		insertionSort();
+		mergeSort();
 		finishTime = System.nanoTime();
 		timeElapsed = finishTime - startTime;
 		System.out.println("Merge: " + timeElapsed);
 		outputToFile();
 	}
-	
-	
-	  public void insertionSort()
-	    {
-	        for (int index = 1; index < data.length; index++)
-	        {
-	            int key = data[index];
-	            int position = index;
-				numberOfPasses++;
-				getProgress();
-	          
-	            while (position > 0 && data[position-1].compareTo(key) > 0)
-	            {
-	                data[position] = data[position-1];
-	                position--;
-	            }
-				
-	            data[position] = key;
-	        }
-	    }
-	  
-	  private void outputToFile() {
-			File outputFile = new File("output.dat");
-			try {
-				PrintWriter outStream = new PrintWriter(new FileWriter(outputFile, true));
-				outStream.append("Merge Sort Thread elapsed in " + timeElapsed + " nanoseconds");
-				outStream.close();
-			} catch (IOException e) {
-				System.out.println("Merge Sort didn't write.");
-			}
-			
+
+	private void outputToFile() {
+		File outputFile = new File("output.dat");
+		try {
+			PrintWriter outStream = new PrintWriter(new FileWriter(outputFile, true));
+			outStream.append("Merge Sort Thread elapsed in " + timeElapsed + " nanoseconds");
+			outStream.close();
+		} catch (IOException e) {
+			System.out.println("Merge Sort didn't write.");
 		}
-	  
-	  public int getProgress()
-	  {
-	  	int result;
-	  	progressCount = 1 - (((double)data.length - (double)numberOfPasses)/(double)data.length);
-	  	result = (int) (progressCount * 100);
-	  	bar.setValue(result);
-	  	return result;
-	  }
+
+	}
+
+	public int getProgress()
+	{
+		int result;
+		progressCount = 1 - (((double)data.length - (double)numberOfPasses)/(double)data.length);
+		result = (int) (progressCount * 100);
+		bar.setValue(result);
+		return result;
+	}
+
+	public void mergeSort()
+	{
+		mergeSort(data, 0, data.length - 1);
+	}
+
+	private void mergeSort(Integer[] data, int min, int max)
+	{
+		if (min < max)
+		{
+			int mid = (min + max) / 2;
+			mergeSort(data, min, mid);
+			mergeSort(data, mid+1, max);
+			merge(data, min, mid, max);
+		}
+	}
+
+	private void merge(Integer[] data, int first, int mid, int last)
+	{
+		int[] temp = new int[data.length];
+		int first1 = first, last1 = mid;
+		int first2 = mid+1, last2 = last; 
+		int index = first1; 
+		while (first1 <= last1 && first2 <= last2)
+		{
+			if (data[first1] < data[first2])
+			{
+				temp[index] = data[first1];
+				first1++;
+			}
+			else
+			{
+				temp[index] = data[first2];
+				first2++;
+			}
+			index++;
+			numberOfPasses++;
+			getProgress();
+		}
+
+		while (first1 <= last1)
+		{
+			temp[index] = data[first1];
+			first1++;
+			index++;
+			//numberOfPasses++;
+			//getProgress();
+		}
+
+		while (first2 <= last2)
+		{
+			temp[index] = data[first2];
+			first2++;
+			index++;
+			//numberOfPasses++;
+			//getProgress();
+		}
+
+		for (index = first; index <= last; index++)
+		{
+			data[index] = temp[index];
+			//numberOfPasses++;
+			//getProgress();
+		}
+	}
 }
